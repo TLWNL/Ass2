07624a1fd95dab979c06102812aaabb1522c460e
Delay67 <43353540+Delay67@users.noreply.github.com>
2018-10-05 20:14:29 +0200

    added hash init

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.math.BigInteger;
+import java.util.HashMap;
 public class Main {
+
 
     private void start() {
         // Create a scanner on System.in
         // While there is input, read line and parse it.
+    	
+    	//the "Set" is a placeholder, this is the type our hasmap will hold im fairly sure it should be the sets though
+    	HashMap<BigInteger, Set> hashTable = new HashMap<BigInteger, Set>();
     }
 
     public static void main(String[] argv) {
         new Main().start();
     }
+    
 }
